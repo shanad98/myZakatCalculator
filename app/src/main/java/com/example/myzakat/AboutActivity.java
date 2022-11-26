db4fe1dc64a7a717f98b66eba02eda35aa43b3fc
Sharifah Nadzirah <nad.leo98@gmail.com>
2022-11-26 17:56:49 +0800

    first commit

---
+++ app/src/main/java/com/example/myzakat/AboutActivity.java
@@ -0,0 +1,18 @@
+package com.example.myzakat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+public class AboutActivity extends AppCompatActivity {
+        TextView textView;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_about);
+            textView = findViewById(R.id.textViewLink);
+            textView.setMovementMethod (LinkMovementMethod.getInstance());
+            }
+}
